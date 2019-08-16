bc21ccc0bf82dfee94253b336f144b29ac418867
Doug Roberts <droberts@suhsd.net>
2019-08-16 14:40:43 -0700

    Initial sharing of project

---
+++ Calculator.java
@@ -0,0 +1,56 @@
+
+/**
+ * Using primitives to calculate
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Calculator
+{
+    // instance variables - replace the example below with your own
+    private int a;
+    private int b;
+
+    /**
+     * Constructor for objects of class Calculator
+     */
+    public Calculator(int a, int b)
+    {
+        // initialise instance variables
+        this.a = a;
+        this.b = b;
+        
+    }
+
+    /**
+     * An example of a method 
+     * param are the values that the method needs to get.
+     * return is a description of the values that should be returned.
+     * 
+     * @param  none
+     * @return the sum of a and b
+     */
+    public int add()
+    {
+        // example
+        return a+b;
+    }
+    
+    public int subtract()
+    {
+        // replace with your code
+        return 7;
+    }
+    
+    public int multiply()
+    {
+        // replace with your code
+        return 7;
+    }
+    
+    public double divide()
+    {
+        // replace with your code
+        return 7;
+    }
+}
